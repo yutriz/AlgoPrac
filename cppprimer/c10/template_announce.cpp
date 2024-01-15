24b011332c67858f05f6ee96b6ed031492dba432
prof <-->
2024-01-16 01:46:38 +0800

    c10

---
+++ cppprimer/c10/template_announce.cpp
@@ -0,0 +1,12 @@
+template <class Type>
+    Type foo(Type, Type);
+template <class Type>
+    Type foo(Type, Type);
+
+template <class T1, class T2>
+    void bar(T1, T2);
+template <typename C1, typename C2>
+    void bar(C1, C2);
+int main() {
+
+}
