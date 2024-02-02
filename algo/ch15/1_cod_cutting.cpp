118363e62a291dccc0c39bb1c8a9eb333c3bffa1
me <none>
2024-02-02 03:44:44 -0800

    no

--- algo/ch15/1_cod_cutting.cpp
+++ algo/ch15/1_cod_cutting.cpp
@@ -1,10 +1,45 @@
 #include <iostream>
 #include <vector>
+#include <limits.h>
 
 using namespace std;
 
+// const vector<int> len={1,2,3,4,5,6,7,8,9,10};
+
+const vector<int> price={0,1,5,8,9,10,17,17,20,24,30};
+
+// max profit of n inches
+const vector<int> max_prof={0,1,5,8,10,13,17,18,22,25,25,30};
+
+
+//  n
+//  i   n-i
+//       i   n-i
+//            ****
+
+
+// first seg: 
+//      len=i profit=p(i)
+//  remain n-i 
+//
+
+// recursive one 
+int f_max_prof(int len) {
+    if (len == 0)
+        return 0;
+    
+    int q = 0; 
+
+    for (int i=1; i<len+1; i++) {
+        cout << "i: "<<i<<" q: "<<q<<endl;
+        q = max(q, price[i]+f_max_prof(len-i));
+    }
+    return q;
+}
+
+
+
 int main() {
-    vector<int> len;
     
     /* 
      * given a steel bar of N inches 
@@ -14,16 +49,8 @@ int main() {
      * calculate the maximum profile
      *
      * */
-
-    for(int i=0; i<10; i++)
-        len[i] = i;
-    
-    vector<int> price = {1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
-
-    
-    // max profile of n inches
-    vector<int> max_prof = {1, 5, 8, 10, 13, 17, 18, 22, 25, 25, 30};
+    cout << f_max_prof(20) << endl;
 
     
 
-
+}
